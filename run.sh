3657f65a5969f7f84250c18cd258ca234103ed8f
yunkyung.lee <yunkyung.lee@navercorp.com>
2020-02-04 08:45:34 +0900

    volume add

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run -d --name nowol-test -p 8030:8030 -p 8031:8031 -p 8032:8032 -p 50070:50070 -p 50075:50075 -p 7070:7070 -p 7979:7979 nowol/hive-3.2.1
+rm -rf ./logs
+
+docker run -d --name hadoop-standalone -v $PWD/logs:/opt/hadoop/hadoop-3.1.2/logs -p 8030:8030 -p 8031:8031 -p 8032:8032 -p 50070:50070 -p 50075:50075 -p 7070:7070 -p 7979:7979 -p 8088:8088 nowol79/hadoop-3.1.2-standalone
